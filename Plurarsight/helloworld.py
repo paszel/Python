eacf52d6aa6e68abead5f8f42235b07c7d619fd3
Pawel Klima <pawel.artur.klima@gmail.com>
2017-05-11 21:01:52 +0000

    Plurarsight start

---
+++ Plurarsight/helloworld.py
@@ -0,0 +1,15 @@
+print ('Hello World!')
+
+def add_numbers(a:int, b:int) -> int:
+    return a + b
+
+print(add_numbers(5,13))
+print("Don't worry about conversions!")
+print(add_numbers(123,10.5))
+print('Casting')
+
+pi = 3.14159
+c = 15
+
+print(int(pi))
+print(float(c))
