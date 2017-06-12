fdf551224975a007e16156a8edf5a53ff1f0d6da
Pawel <pawel.artur.klima@gmail.com>
2017-06-12 23:59:21 +0200

    little improvement

--- Plurarsight/GuessNumber/guess.py
+++ Plurarsight/GuessNumber/guess.py
@@ -3,12 +3,28 @@ import random
 print("Hello, what is you favoirte number?")
 number = input()
 
-print("Your favorite number is "+number)
+print("Your favorite number is " + number)
 
 minNumber = 1
 maxNumber = 100
 
 magicNumber = random.randint(minNumber, maxNumber)
 
-message = "This magic number is between (0) and (1)"
-print(message.format(minNumber, maxNumber)
+message = "This magic number is between {0} and {1}"
+print(message.format(minNumber, maxNumber))
+
+found = False
+
+while not found:
+    print('Guess what it is?')
+    guess = int(input())
+    if guess == magicNumber:
+        found = True
+    if guess < magicNumber:
+        print('To low')
+    if guess > magicNumber:
+        print('To high')
+
+
+
+print("You got it!")
